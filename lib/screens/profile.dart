6a897493d58f1d4164494cfbc16b4d086b594f0d
MUHAMMAD FAIZ BIN ANUAR 198921 <198921@student.upm.edu.my>
2021-01-20 00:36:41 +0800

    profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -8,6 +8,59 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: new Center(
+            child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 25,
+            ),
+            Image.asset(
+              'assets/profile1.png',
+              width: 200,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "User Name",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Balance : ",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            RaisedButton(
+              textColor: Colors.white,
+              color: Colors.purple,
+              child: Text('Add Location'),
+              onPressed: () {},
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            RaisedButton(
+              textColor: Colors.white,
+              color: Colors.purple,
+              child: Text('Log Out'),
+              onPressed: () {},
+            ),
+          ],
+        )));
   }
 }
